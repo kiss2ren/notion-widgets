2eda82fc9369284d259363b63dc16de22a68bc0a
kiss2ren <raeofsunshine435@gmail.com>
2026-02-14 22:13:40 -0600

    Create main.js

---
+++ js/pages/main.js
@@ -0,0 +1,11 @@
+import "../css/main.scss";
+import "./pages/tumblr-tag-list-tool";
+import "./pages/notion-widgets";
+import "./pages/pinterest-board";
+import "./pages/vocabulary-manager";
+import "./pages/manual-list-sorter";
+import "./pages/notion-to-quizlet";
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("hewwo :3");
+});
