89f565df2ebfe41cfbaabf48b70161c692193619
kiss2ren <raeofsunshine435@gmail.com>
2026-03-10 04:37:02 -0500

    Update script.js

--- njz/search/script.js
+++ njz/search/script.js
@@ -1,1 +1,10 @@
-
+var typing=new Typed(".typing", {
+    strings: ["", "Who is NJZ*",
+ "NJZ is <i>trendy*</i>", "NJZ is creative*", "NJZ is <span style='font-style: italic'>jeanius*</span>", "NJZ is <span style='font-style:italic'>sweet*</span>", "NJZ is <span style='font-weight: bold'>fun*</span>"],
+    typeSpeed: 125,
+    backSpeed: 80,
+    backDelay: 1000,
+    smartBackspace: true,
+    loop: true,
+    cursorChar: '', // <--- Prevents Typed.js from inserting '|'
+});
